3ad1460814236054986f64a9c4444982f120cdfe
r.danilov <r.danilov@zebrains.team>
2020-01-22 10:47:19 +0400

    ДЗ 5 урока

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,33 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id =:id AND m.user.id =:user_id")
+    int delete(@Param("id") int id, @Param("user_id") int userId);
+
+    List<Meal> getAllByUserIdOrderByDateTimeDesc(int userId);
+
+    Meal getByIdAndUserId(int id, int userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id =:user_id AND m.dateTime >= :start_date AND m.dateTime < :end_date ORDER BY m.dateTime DESC")
+    List<Meal> getAllByUserIdAndGetBetweenDates(
+            @Param("start_date") LocalDateTime startDate,
+            @Param("end_date") LocalDateTime endDate,
+            @Param("user_id") int userId
+    );
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -3,7 +3,12 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.getEndExclusive;
+import static ru.javawebinar.topjava.util.DateTimeUtil.getStartInclusive;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,28 +19,39 @@ public class DataJpaMealRepository implements MealRepository {
     @Autowired
     private CrudMealRepository crudRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
+        }
+        meal.setUser(userRepository.get(userId));
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.getByIdAndUserId(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAllByUserIdOrderByDateTimeDesc(userId);
     }
 
     @Override
     public List<Meal> getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
-        return null;
+        return crudRepository.getAllByUserIdAndGetBetweenDates(
+                getStartInclusive(startDate),
+                getEndExclusive(endDate),
+                userId
+        );
     }
 }
